e95a693e4d045cae5484b1585f701129cf4ed445
Wemeson Araujo <wemeson@gmail.com>
2024-09-13 04:12:26 -0400

    add fontFamily and fixed error in select font bitween ios and android

--- App.tsx
+++ App.tsx
@@ -1,5 +1,3 @@
-
-
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import { Text } from './src/components/Text/Text';
@@ -7,13 +5,12 @@ import { Text } from './src/components/Text/Text';
 function App(): React.JSX.Element {
   return (
     <SafeAreaView>
-        <Text preset="paragraphMedium">Teste</Text>
-        <Text preset="paragraphMedium">Teste</Text>
+        <Text preset="headingLarge" bold >Testecsdcsc</Text>
+        <Text preset="headingLarge" semiBold >Testecsdcsc</Text>
+        <Text preset="headingLarge" italic>Teste</Text>
         <Text preset="paragraphMedium">Teste</Text>
     </SafeAreaView>
   );
 }
-
-
 
 export default App;
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -3,21 +3,48 @@ import { Text as RNText, TextProps as RNTextProps, TextStyle } from 'react-nativ
 
 interface TextProps extends RNTextProps {
   preset?: TextVariants;
+  bold?: boolean;
+  italic?: boolean;
+  semiBold?: boolean;
 }
 
 export function Text({
   children,
   preset = 'paragraphMedium',
   style,
+  bold,
+  italic,
+  semiBold,
   ...rest
 }: TextProps) {
 
+  const fontFamily = getFontFemily(bold, italic, semiBold);
+
   return (
-    <RNText style={[$fontSizes[preset], style]} {...rest}>
+    <RNText style={[$fontSizes[preset], { fontFamily: fontFamily }, style]} {...rest}>
       {children}
     </RNText>
   );
 }
+
+function getFontFemily(bold?: boolean, italic?: boolean, semiBold?: boolean) {
+  switch (true) {
+    case (bold && italic):
+      return $fontFamily.boldItalic;
+    case (bold):
+      return $fontFamily.bold;
+    case (italic):
+      return $fontFamily.italic;
+    case (semiBold && italic):
+      return $fontFamily.mediumItalic;
+    case (semiBold):
+      return $fontFamily.medium;
+    default:
+      return $fontFamily.regular;
+  }
+}
+
+
 
 type TextVariants =
   | 'headingLarge'
@@ -40,4 +67,17 @@ const $fontSizes: Record<TextVariants, TextStyle> = {
 
   paragraphCaption: { fontSize: 12, lineHeight: 16.8 },
   paragraphCaptionSmall: { fontSize: 10, lineHeight: 14 },
+};
+
+const $fontFamily = {
+  black: 'Satoshi-Black',
+  blackItalic: 'Satoshi-BlackItalic',
+  bold: 'Satoshi-Bold',
+  boldItalic: 'Satoshi-BoldItalic',
+  italic: 'Satoshi-Italic',
+  light: 'Satoshi-Light',
+  lightItalic: 'Satoshi-LightItalic',
+  medium: 'Satoshi-Medium',
+  mediumItalic: 'Satoshi-MediumItalic',
+  regular: 'Satoshi-Regular',
 };
